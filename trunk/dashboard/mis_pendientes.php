1aedc507b2e3a83cfe5a3cf1da1b747d665cb217
emilioplatzer <emilioplatzer@a0b0f95c-8119-8eb6-b2f4-cdf7e6dbf4d2>
2013-10-19 14:06:42 +0000

    Agrego los archivos que faltaban

--- trunk/dashboard/mis_pendientes.php
+++ trunk/dashboard/mis_pendientes.php
@@ -5,7 +5,10 @@
 </head>
 <table id=encabezado><tr><td>
     <img id=logo src=logo-hi-res.png>
-<td style="text-align:right; vertical-align:bottom"><table><tr><td style=text-align:right><img src=google-bar.png>
+<td style="text-align:right; vertical-align:bottom"><table>
+    <tr><td style=text-align:right><input id=user><input id=pass type=password><input id=boton_login value='Sing In' type=button>
+    <tr><td style=height:40px>
+    <tr><td style=text-align:right><img src=google-bar.png>
     <tr><td id=frase_titulo style=text-align:right>Tareas pendientes por responder <span id=nombre_usuario contenteditable>ingresar G+</span>
     </table>
 <tr style="height:4px; padding:0px; margin:0px;"><td style='font-size:1px; background-color:#EC0000; height:4px; padding:0px; margin:0px; text-align:right' colspan=2><img src=rayita.png style=" padding:0px; margin:0px;"></td></tr>
